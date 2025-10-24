ae6df673cf9aee7a74d147c76865a56e8ad65b9f
vanshitm12 <vanshitmalik12@gmail.com>
2025-10-25 00:01:24 +0530

    Add signup route to auth router

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { register, login } = require('../controllers/authController');
 
 router.post('/register', register);
+router.post('/signup', register); 
 router.post('/login', login);
 
 module.exports = router;
