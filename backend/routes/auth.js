a54a20a4a0805eeb215d11c4333c0a0122470ebe
vanshitm12 <vanshitmalik12@gmail.com>
2025-10-25 14:20:07 +0530

    Adding Comments

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,6 +4,6 @@ const { register, login } = require('../controllers/authController');
 
 router.post('/register', register);
 router.post('/signup', register);  // added this for better clarity
-router.post('/login', login);
+router.post('/login', login); // routes
 
 module.exports = router;
